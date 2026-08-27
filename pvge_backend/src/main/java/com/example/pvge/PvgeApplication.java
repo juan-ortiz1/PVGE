e5e40ea538a2e264a8400d75fd4a3ea3b4b4cd8c
juan-ortiz1 <juandavidom4@gmail.com>
2026-08-26 22:16:40 -0500

    feat: Primer commit. Spring Boot + PostgreSQL

---
+++ pvge_backend/src/main/java/com/example/pvge/PvgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.pvge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvgeApplication.class, args);
+	}
+
+}
